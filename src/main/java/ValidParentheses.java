944bdc371e178781b7ad9a8435539c421b31e350
Ray Chung <239297442@qq.com>
2020-02-14 22:45:00 +0800

    232 & 225 & 20

---
+++ src/main/java/ValidParentheses.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+/**
+ * Created on 2020/2/13.
+ *
+ * @author ray
+ */
+
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+//
+// 有效字符串需满足：
+//
+//
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+//
+//
+// 注意空字符串可被认为是有效字符串。
+//
+// 示例 1:
+//
+// 输入: "()"
+//输出: true
+//
+//
+// 示例 2:
+//
+// 输入: "()[]{}"
+//输出: true
+//
+//
+// 示例 3:
+//
+// 输入: "(]"
+//输出: false
+//
+//
+// 示例 4:
+//
+// 输入: "([)]"
+//输出: false
+//
+//
+// 示例 5:
+//
+// 输入: "{[]}"
+//输出: true
+// Related Topics 栈 字符串
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if ("".equalsIgnoreCase(s)) {
+            return true;
+        }
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char aChar : chars) {
+            if ('(' == aChar || '[' == aChar || '{' == aChar) {
+                stack.push(aChar);
+            } else {
+                Character top = stack.empty() ? null : stack.pop();
+                if (top == null) {
+                    return false;
+                }
+                if (')' == aChar) {
+                    if (!top.equals('(')) {
+                        return false;
+                    }
+                } else if (']' == aChar) {
+                    if (!top.equals('[')) {
+                        return false;
+                    }
+                } else {
+                    if (!top.equals('{')) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
